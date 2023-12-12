f2d60d3a91dcbc5b1e18ffae43bf32816c284954
vincent0616 <coji204396@gmail.com>
2023-12-12 11:36:21 +0900

    Initial commit

---
+++ src/main/java/com/example/aws_test/AwsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aws_test/AwsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
